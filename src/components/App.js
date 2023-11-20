eae832bdfd44a9a2903bcbee379e321c8c78b3b7
unknown <you@example.com>
2023-11-21 00:05:48 +0100

    Zamiana przycisków potwierdzających zadanie na funkcjonalność drag and drop react

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 import "./App.css";
 import React from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 const App = () => {
   const [counter, setCounter] = useState(3);
@@ -49,15 +51,17 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <h1>TODO APP</h1>
-      <AddTask add={addTask} />
-      <TaskList
-        tasks={allTasks}
-        delete={deleteTask}
-        change={changeTaskStatus}
-      />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <h1>TODO APP</h1>
+        <AddTask add={addTask} />
+        <TaskList
+          tasks={allTasks}
+          delete={deleteTask}
+          change={changeTaskStatus}
+        />
+      </div>
+    </DndProvider>
   );
 };
 
